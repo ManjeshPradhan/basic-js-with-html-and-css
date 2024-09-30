fb26d61f7124796592495af6788dfcdf7ec3f497
manjeshPradhan <manjeshpradhan777@gmail.com>
2024-09-30 22:10:10 +0545

    prevent default used to prevent refresh of page

--- Multiplication_App/index.js
+++ Multiplication_App/index.js
@@ -1,5 +1,6 @@
-const num1 = Math.ceil(Math.random() * 10);
-const num2 = Math.ceil(Math.random() * 10);
+let num1;
+let num2;
+let correctAns;
 
 const questionEl = document.getElementById("question");
 
@@ -8,30 +9,31 @@ const inputEl = document.getElementById("input");
 const formEl = document.getElementById("form");
 
 const scoreEl = document.getElementById("score");
-
-let score = JSON.parse(localStorage.getItem("score"));
+let score
 
 if (!score) {
   score = 0;
 }
 
-scoreEl.innerText = `score: ${score}`;
+function changeVariables() {
+  num1 = Math.ceil(Math.random() * 10);
+  num2 = Math.ceil(Math.random() * 10);
+  questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+  correctAns = num1 * num2;
+  scoreEl.innerText = `score: ${score}`;
+}
 
-questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+changeVariables()
 
-const correctAns = num1 * num2;
-
-formEl.addEventListener("submit", () => {
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault()
   const userAns = +inputEl.value;
   if (userAns === correctAns) {
     score++;
-    updateLocalStorage();
   } else {
     score--;
-    updateLocalStorage();
   }
-});
+  inputEl.value = null;
+  changeVariables()
 
-function updateLocalStorage() {
-  localStorage.setItem("score", JSON.stringify(score));
-}
+});
